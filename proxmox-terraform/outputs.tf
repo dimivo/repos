6ed92c46e532f63b822bb249c39f2ea1c8d8954c
Dimivo <ivaylodimov.id@gmail.com>
2026-05-21 11:13:40 +0300

    Remove proxmox-terraform to its own repo

--- proxmox-terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_ids" {
-  description = "IDs of the created VMs"
-  value       = proxmox_virtual_environment_vm.vm[*].vm_id
-}
-
-output "vm_names" {
-  description = "Names of the created VMs"
-  value       = proxmox_virtual_environment_vm.vm[*].name
-}
